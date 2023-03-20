ecdb9f4f1b8ada8749700bfac36cf0222a982a2b
jacobwulffwold <61497974+jacobwulffwold@users.noreply.github.com>
2023-03-20 15:41:44 +0100

    Slicing and interpolating data

--- download_data.py
+++ download_data.py
@@ -249,12 +249,6 @@ def extract_3D(data_code, start_date, end_date):
     return time, latitude, longitude, terrain, x, y, z, u, v, w, theta, tke, td
 
 
-
-x_dict={"start": 4, "max": -3, "step": 1},
-
-z_dict={"start": -1, "max": -41, "step": -1},
-
-
 def slice_data(
     terrain,
     x,
@@ -302,136 +296,181 @@ def slice_data(
         x_dict["start"] : x_dict["max"] : x_dict["step"],
     ]
 
-    return terrain, X, Y, z, u, v, w
+    return (
+        terrain,
+        100000*x[x_dict["start"] : x_dict["max"] : x_dict["step"]],
+        100000*y[x_dict["start"] : x_dict["max"] - 1 : x_dict["step"]],
+        X,
+        Y,
+        z,
+        u,
+        v,
+        w,
+    )
 
 
-def interpolate_cartesian_3D(X_input, Y_input, Z_input, output_dimension=128):
-
-    XYZsigma_flat = np.vstack((X_input.flatten(), Y_input.flatten(), Z_input.flatten())).T
-    y_wind_field = np.vstack((u[0].flatten(), v[0].flatten(), w[0].flatten())).T
-
+def interpolate_cartesian_3D(
+    X_input, Y_input, Z_input, u_input, v_input, w_input, x, y, time_index
+):
+    XYZ_sigma_flat = np.vstack(
+        (X_input.flatten(), Y_input.flatten(), Z_input.flatten())
+    ).T
+    y_wind_field = np.vstack(
+        (u_input[time_index].flatten(), v_input[time_index].flatten(), w_input[time_index].flatten())
+    ).T
 
     kernel = RBF(length_scale=10, length_scale_bounds=(1e-2, 1e2)) + WhiteKernel(
         noise_level=1e-5, noise_level_bounds=(1e-10, 1e1)
     )
 
-
     gpr = GaussianProcessRegressor(kernel=kernel, alpha=0.1, normalize_y=True)
 
-    gpr.fit(XYZsigma_flat, y_wind_field)
+    gpr.fit(XYZ_sigma_flat, y_wind_field)
 
     X_cartesian, Y_cartesian, Z_cartesian = np.meshgrid(
-        100000 * x[4:-3], 100000 * y[4:-4], np.arange(0, 2000, 128)
+        x, y, np.linspace(np.average(Z_input[:, :, 0]), np.average(Z_input[:, :, -1]), x.size)
     )
 
-    X_flat_cartesian = np.vstack(
+    XYZ_flat_cartesian = np.vstack(
         (X_cartesian.flatten(), Y_cartesian.flatten(), Z_cartesian.flatten())
     ).T
 
-    u_cart, v_cart, w_cart = gpr.predict(X_flat_cartesian)
+    pred_wind_field_cartesian = gpr.predict(XYZ_flat_cartesian)
+
+    u_cart = pred_wind_field_cartesian[:,0].reshape(x.size,x.size,x.size)
+    v_cart = pred_wind_field_cartesian[:,1].reshape(x.size,x.size,x.size)
+    w_cart = pred_wind_field_cartesian[:,2].reshape(x.size,x.size,x.size)
+    
+    return X_cartesian, Y_cartesian, Z_cartesian, u_cart, v_cart, w_cart
+
+def plot_field(X,
+        Y,
+        Z,
+        z_plot_scale,
+        u,
+        v,
+        w,
+        terrain=np.asarray([])):
+    
+    mlab.quiver3d(
+        X,
+        Y,
+        z_plot_scale * Z,
+        u,
+        v,
+        w,
+    )    
+
+    if terrain.any():
+        mlab.surf(X[:,:,0].T, Y[:,:,0].T, z_plot_scale*terrain.T, colormap="black-white")
+
+    mlab.show()
+
+if __name__ == "__main__":
+    
+    data_code = "simra_BESSAKER_"
+    start_date = date(2018, 4, 1)  # 1,2
+    end_date = date(2018, 4, 3)  #
+    download_Bessaker_data(start_date, end_date, "./")
+    # combine_files(data_code,start_date,end_date,outfilename='temp.pkl')
+
+    time, latitude, longitude, terrain, x, y, z, u, v, w, theta, tke, td = extract_3D(
+        data_code, start_date, end_date
+    )
+
+    [print(x.shape) for x in [terrain, x, y, z, u, v, w]]
+
+    z_plot_scale = 5
+    time_index = 41
+    X_DICT = {"start": 4, "max": -120, "step": 1}
+    Z_DICT = {"start": -1, "max": -20, "step": -2}
+
+    terrain, x, y, X, Y, z, u, v, w = slice_data(terrain, x, y, z, u, v, w, X_DICT, Z_DICT)
 
 
+    [print(x.shape) for x in [terrain, x, y, X, Y, z, u, v, w]]
 
-data_code = "simra_BESSAKER_"
-start_date = date(2018, 4, 1)  # 1,2
-end_date = date(2018, 4, 3)  #
-download_Bessaker_data(start_date, end_date, "./")
-# combine_files(data_code,start_date,end_date,outfilename='temp.pkl')
+    # mlab.quiver3d(X, Y, z_plot_scale*z, u[time_index], v[time_index], w[time_index])
+    # mlab.show()
 
-time, latitude, longitude, terrain, x, y, z, u, v, w, theta, tke, td = extract_3D(
-    data_code, start_date, end_date
-)
+    (
+        X_cartesian,
+        Y_cartesian,
+        Z_cartesian,
+        u_cart,
+        v_cart,
+        w_cart,
+    ) = interpolate_cartesian_3D(X, Y, z, u, v, w, x, y, time_index)
 
-[print(x.shape) for x in [terrain, x, y, z, u, v, w]]
+    plot_field(X, Y, z, z_plot_scale, u[time_index],v[time_index],w[time_index],terrain)    
 
-z_max = z[0, 0, :].size
-z_lim = z_max
-z_step = 35
-z_start = 1
-z_plot_scale = 3
-
-
-terrain, X, Y, z, u, v, w = slice_data(terrain, x, y, z, u, v, w)
+    # X_2D, Y_2D = np.meshgrid(x,y)
 
 
-[print(x.shape) for x in [terrain, X, Y, z, u, v, w]]
+    # v = np.ma.filled(v.astype(float), np.nan)
+    # v = v[:, 4:-4, 4:-3, ::-1]
+
+    # w = np.ma.filled(w.astype(float), np.nan)
+    # w = w[:, 4:-4, 4:-3, ::-1]
+
+    # terrain = np.ma.filled(terrain.astype(float), np.nan)
+    # terrain = terrain[4:-4, 4:-3]
+
+    # z = z[4:-4, 4:-3, ::-1]
+
+    # X, Y = 100000 * X[4:-4, 4:-3, :], 100000 * Y[4:-4, 4:-3, :]
+    # X_2D, Y_2D = 100000*X_2D[4:-4, 4:-3].transpose(), 100000*Y_2D[4:-4, 4:-3].transpose()
 
 
-# X_2D, Y_2D = np.meshgrid(x,y)
+    # mlab.quiver3d(X[:,:, z_start:z_lim:z_step],Y[:,:, z_start:z_lim:z_step],z_scale*z[:,:, z_start:z_lim:z_step],u_nomask[time_index][:, :, z_start:z_lim:z_step], v_nomask[time_index][:, :, z_start:z_lim:z_step], w_nomask[time_index][:,:, z_start:z_lim:z_step], mask_points=1)
+    # mlab.surf(X_2D, Y_2D, z_scale*terrain_nomask.transpose(), colormap="black-white")
 
-# ax = plt.figure().add_subplot(projection='3d')
-
-# ax.quiver(X[::10, ::10, ::10],Y[::10, ::10, ::10],z[::10, ::10, ::10],u[0][::10, ::10, ::10], v[0][::10, ::10, ::10], w[0][::10, ::10, ::10])
-
-# mlab.quiver3d(X,Y,z, u[0], v[0], w[0])
-
-
-# v = np.ma.filled(v.astype(float), np.nan)
-# v = v[:, 4:-4, 4:-3, ::-1]
-
-# w = np.ma.filled(w.astype(float), np.nan)
-# w = w[:, 4:-4, 4:-3, ::-1]
-
-# terrain = np.ma.filled(terrain.astype(float), np.nan)
-# terrain = terrain[4:-4, 4:-3]
-
-# z = z[4:-4, 4:-3, ::-1]
-
-# X, Y = 100000 * X[4:-4, 4:-3, :], 100000 * Y[4:-4, 4:-3, :]
-# X_2D, Y_2D = 100000*X_2D[4:-4, 4:-3].transpose(), 100000*Y_2D[4:-4, 4:-3].transpose()
+    # # for i in range(z_start,z_max,z_step):
+    # #     mlab.mesh(X_2D, Y_2D, z_scale*z[:, :, i].transpose(), scalars=np.sqrt(u_nomask[time_index][:,:,i]**2+v_nomask[time_index][:,:,i]**2+w_nomask[time_index][:,:,i]**2).transpose())
+    # mlab.show()
 
 
-# mlab.quiver3d(X[:,:, z_start:z_lim:z_step],Y[:,:, z_start:z_lim:z_step],z_scale*z[:,:, z_start:z_lim:z_step],u_nomask[0][:, :, z_start:z_lim:z_step], v_nomask[0][:, :, z_start:z_lim:z_step], w_nomask[0][:,:, z_start:z_lim:z_step], mask_points=1)
-# # mlab.surf(X_2D, Y_2D, z_scale*terrain_nomask.transpose(), colormap="black-white")
+    # X, Y = np.meshgrid(x, y)
+    # plt.contour(z)
+    # plt.contourf(u[1,:,:],cmap='hsv')
+    # plt.quiver(u[10,:,:],v[10,:,:],scale=200)
+    # plt.show()
+    # print(np.max(u),np.max(v),np.max(w),np.max(theta))
+    # for i in tqdm(range(135)):
+    #     for j in range(136):
+    #         for k in range(41):
+    #             vars[:,k,j,i] = np.var(z[:,k,j,i])
 
-# # for i in range(z_start,z_max,z_step):
-# #     mlab.mesh(X_2D, Y_2D, z_scale*z[:, :, i].transpose(), scalars=np.sqrt(u_nomask[0][:,:,i]**2+v_nomask[0][:,:,i]**2+w_nomask[0][:,:,i]**2).transpose())
-# mlab.show()
-
-
-# X, Y = np.meshgrid(x, y)
-# plt.contour(z)
-# plt.contourf(u[1,:,:],cmap='hsv')
-# plt.quiver(u[10,:,:],v[10,:,:],scale=200)
-# plt.show()
-# print(np.max(u),np.max(v),np.max(w),np.max(theta))
-# for i in tqdm(range(135)):
-#     for j in range(136):
-#         for k in range(41):
-#             vars[:,k,j,i] = np.var(z[:,k,j,i])
-
-# print(vars)
-# print(np.average(vars))
-# print(np.median(vars))
-# print(np.max(vars))
+    # print(vars)
+    # print(np.average(vars))
+    # print(np.median(vars))
+    # print(np.max(vars))
 
 
+    """
+    delta_x = (X[135, 134] - X[0, 0])*10**5
+    delta_y = (Y[135, 134] - Y[0, 0])*10**5
+    X_res = delta_x/136
+    Y_res = delta_y/135
+    """
+    # saving u, v, w
+    with open("2018_apr.pickle", "wb") as f:
+        pickle.dump([u, v, w], f)
 
 
-"""
-delta_x = (X[135, 134] - X[0, 0])*10**5
-delta_y = (Y[135, 134] - Y[0, 0])*10**5
-X_res = delta_x/136
-Y_res = delta_y/135
-"""
-# saving u, v, w
-with open("2018_apr.pickle", "wb") as f:
-    pickle.dump([u, v, w], f)
+    """
+    with open('2018_january.pickle', 'rb') as f:
+    u_jan, v_jan, w_jan = pickle.load(f)
+    with open('2018_february.pickle', 'rb') as f:
+    u_feb, v_feb, w_feb = pickle.load(f)
+    with open('2018_march.pickle', 'rb') as f:
+    u_mar, v_mar, w_mar = pickle.load(f)
+    with open('2018_apr.pickle', 'rb') as f:
+    u_apr, v_apr, w_apr = pickle.load(f)
 
-
-"""
-with open('2018_january.pickle', 'rb') as f:
-   u_jan, v_jan, w_jan = pickle.load(f)
-with open('2018_february.pickle', 'rb') as f:
-   u_feb, v_feb, w_feb = pickle.load(f)
-with open('2018_march.pickle', 'rb') as f:
-   u_mar, v_mar, w_mar = pickle.load(f)
-with open('2018_apr.pickle', 'rb') as f:
-   u_apr, v_apr, w_apr = pickle.load(f)
-
-u = np.concatenate((u_jan,u_feb,u_mar, u_apr), axis = 0)
-v = np.concatenate((v_jan,v_feb,v_mar, v_apr), axis = 0)
-w = np.concatenate((w_jan,w_feb,w_mar, w_apr), axis = 0)
-with open('download_data.pickle', 'wb') as f:
-    pickle.dump([u, v, w], f)
-"""
+    u = np.concatenate((u_jan,u_feb,u_mar, u_apr), axis = 0)
+    v = np.concatenate((v_jan,v_feb,v_mar, v_apr), axis = 0)
+    w = np.concatenate((w_jan,w_feb,w_mar, w_apr), axis = 0)
+    with open('download_data.pickle', 'wb') as f:
+        pickle.dump([u, v, w], f)
+    """
